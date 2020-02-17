13d748b826049b65e4b4ee59c8b838f5d3e7086f
domrau <raulinaitisdomas@gmail.com>
2020-02-17 17:50:34 +0200

    - Demo level

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private int value = 1;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player touched me.. (" + gameObject.name + ")");
+            collision.gameObject.GetComponent<Player>().AddCoins(value);
+            value = 0;
+            Destroy(this.gameObject);
+        }        
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float health = 100;
+    public int coins = 0;
+    public TextMeshProUGUI coinText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        coins = 0;
+        AddCoins(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCoins(int amount)
+    {
+        coins += amount;
+        coinText.text = "Coins: " + coins;
+    }
+}
